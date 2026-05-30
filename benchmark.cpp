bf14e07166f0beb7bddce611799274c46eae1a04
Shayaan <shayaanshahzad64@gmail.com>
2026-05-30 04:28:53 +0300

    initial project setup

---
+++ benchmark.cpp
@@ -0,0 +1,12 @@
+﻿// benchmark.cpp : Defines the entry point for the application.
+//
+
+#include "benchmark.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
